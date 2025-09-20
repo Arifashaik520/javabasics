6bfbc1aa4d00a8a987f13024462947a6fd815006
Arifashaik520 <arifashaik9347@gmail.com>
2025-09-20 11:24:09 +0530

    Multipleinterfaces.java

--- javaclasses/src/com/tnsif/interfaces/Multipleinterfaces.java
+++ javaclasses/src/com/tnsif/interfaces/Multipleinterfaces.java
@@ -5,7 +5,10 @@ public class Multipleinterfaces {
 	public static void main(String[] args) 
 	{
 		Demo obj = new Demo();
-		obj.print();
-		obj.show();
+        obj.print();
+        obj.show();
+        obj.draw();
+        obj.play();
 		}
 }
+
