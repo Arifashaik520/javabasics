b97f18212e340b2c7bac947310093bbc7a228758
ARIFA <ARIFA@LAPTOP-FPDDKEUO>
2025-09-25 22:32:24 +0530

    comitted

---
+++ javaclasses/src/com/tnsif/arrays_Strings/Strings.java
@@ -0,0 +1,14 @@
+package com.tnsif.arrays_Strings;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		String a="tkr";
+		System.out.println(a.toUpperCase());
+		System.out.println(a.length());
+		System.out.println(a);
+
+
+	}
+
+}
---
+++ javaclasses/src/com/tnsif/arrays_Strings/arrayDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.arrays_Strings;
+
+public class arrayDemo {
+
+	public static void main(String[] args) {
+		int[] arr=new int[3];
+		arr[0]=4;
+		arr[1]=5;
+		arr[2]=8;
+		for(int i=0;i<arr.length;i++) 
+		{
+			System.out.println(arr[i]+" ");
+		}
+
+   }
+}
