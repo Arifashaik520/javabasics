07c2eacec6c2ebbca56470fe96ea8332fba8e602
ARIFA <ARIFA@LAPTOP-FPDDKEUO>
2025-09-25 22:08:43 +0530

    commited strings and arrays

---
+++ javaclasses/src/com/tnsif/arraysStrings1/Jaggedarray.java
@@ -0,0 +1,18 @@
+package com.tnsif.arraysStrings1;
+
+public class Jaggedarray {
+
+	public static void main(String[] args) {
+		int[][] a1= {{1,2,3,4},{5,6},{7,8,9}};
+		for(int x[]:a1) {
+			for(int y:x) {
+				System.out.print(y+" ");	
+			}
+			System.out.println();
+		}
+		
+
+
+	}
+
+}
---
+++ javaclasses/src/com/tnsif/arraysStrings1/Stringbuilderdemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.arraysStrings1;
+
+public class Stringbuilderdemo {
+
+	public static void main(String[] args) {
+		StringBuilder SB=new StringBuilder("tkrcet");
+		System.out.println(SB.append(" hello"));
+		System.out.println(SB.length());
+		System.out.println(SB.charAt(4));
+		System.out.println(SB);
+
+	}
+
+}
