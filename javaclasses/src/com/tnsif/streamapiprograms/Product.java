453c5885efc7dd8dfb97f1746a2ed1a72b87a66f
ARIFA <ARIFA@LAPTOP-FPDDKEUO>
2025-10-08 21:39:57 +0530

    committed

---
+++ javaclasses/src/com/tnsif/streamapiprograms/Product.java
@@ -0,0 +1,17 @@
+package com.tnsif.streamapiprograms;
+
+public class Product {
+	int id;
+	String name;
+	float price;
+	
+	public Product(int id, String name, float price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+	
+	
+
+}
